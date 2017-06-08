726f11164e5c282370dc5170479781340a231567
rhm <ucabrhm@ucl.ac.uk>
2017-06-08 22:46:16 +0100

    piece of shit

--- offchain/rnggen.go
+++ offchain/rnggen.go
@@ -14,21 +14,18 @@ func main() {
   pmod4 := big.NewInt(0)
   pmod4.Mod(p, four)
   fmt.Println(pmod4)
-  n := big.NewInt(0)
-  n.SetString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141", 16)
-  nmod4 := big.NewInt(0)
-  nmod4.Mod(p, four)
-  fmt.Println(nmod4)
   */
 
+  x := big.NewInt(0)
+  x.SetString("fff23423424234234234234ffffffffffff765456898765434567654323ff", 16)
+  // this should be for i < some set parameter making computation take 60 seconds :(
+  for i := 0; i < 123; i++ {
   // x is the blockhash in our case
-
   // go's built in ModSqrt already uses tonelli-shanks algo for square rooting
   // it's the currently best know slow generation & fast verification function
-  for something {
-    y := big.NewInt(0)
-    y.ModSqrt(x, p)
+    x.ModSqrt(x, p)
+    fmt.Println(x)
   }
 
   // we're gonna make time for generation vs cost of verification when i'm finished :)
-
+}
