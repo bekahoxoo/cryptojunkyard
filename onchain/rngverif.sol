218b23a8d72af7561d3cc29131d4e16f191ec97a
rhm <ucabrhm@ucl.ac.uk>
2017-06-11 23:29:37 +0100

    storing

--- onchain/rngverif.sol
+++ onchain/rngverif.sol
@@ -6,6 +6,7 @@ contract rngverif {
     uint256 prevblock = block.number - 1;
     //uint256 x = uint256(block.blockhash(prevblock));
     uint256 x = 0xfff23423424234234234234ffffffffffff765456898765434567654323ff;
+    uint256 crand;
 
     function verification (uint256 random) returns (bool) {
         for (uint256 i = 1; i < 10; i++) {
@@ -15,6 +16,7 @@ contract rngverif {
         }
 
         if (random == x) {
+            crand = random;
             return true;
         }
     return false;
