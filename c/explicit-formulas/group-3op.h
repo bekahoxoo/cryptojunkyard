c10f5772de5b992a81c0c9d4cfb4e81e2a009260
Rebekah <github@rbkhmrcr.com>
2019-09-16 16:02:08 -0700

    rearranging

---
+++ c/explicit-formulas/group-3op.h
@@ -0,0 +1,49 @@
+#include <stdbool.h>
+
+#define field_BYTES 96
+#define field_BITS 753
+
+#define scalar_uint64 12
+#define scalar_BYTES 96
+#define scalar_BITS 753
+#define group_BYTES 288
+#define affine_BYTES 192
+
+typedef unsigned char field[field_BYTES];
+typedef unsigned char scalar[scalar_BYTES];
+
+typedef struct affine {
+  field x;
+  field y;
+} affine;
+
+typedef struct group {
+  field X;
+  field Y;
+  field Z;
+} group;
+
+extern const field field_modulus;
+extern const field field_zero;
+extern const field field_one;
+extern const field group_coeff_a;
+extern const field group_coeff_b;
+extern const struct group group_zero;
+extern const struct group group_one;
+
+void field_add(field c, const field a, const field b);
+void field_sub(field c, const field a, const field b);
+void field_mul(field c, const field a, const field b);
+void field_dbl(field c, const field a);
+void field_inv(field c, const field a);
+
+void scalar_add(scalar c, const scalar a, const scalar b);
+void scalar_mul(scalar c, const scalar a, const scalar b);
+
+void affine_to_projective(group *r, const affine *p);
+void projective_to_affine(affine *r, const group *p);
+
+void group_add(group *r, const group *p, const group *q);
+void group_madd(group *r, const group *p, const group *q);
+void group_double(group *r, const group *p);
+void group_scalar_mul(group *r, const scalar k, const group *p);
